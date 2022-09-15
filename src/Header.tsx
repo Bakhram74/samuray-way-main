39675e2454f4ae05781c043e5d87fcc17298c5e4
Bakhram <abdurakhimov93@list.ru>
2022-09-15 21:25:15 +0300

    lessons 1-9

---
+++ src/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header = () => {
+    return <div>
+        <p><a href={"#s"}>Home</a></p>
+        <p><a href={"#s"}>News Feed</a></p>
+        <p><a href={"#s"}>Messages</a></p>
+
+    </div>
+
+}
